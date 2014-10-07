737eb1f915a4b2498b23fd6db457ca8635f1316c
Walan Worachet <wworachet@Walans-MacBook-Air-2.local>
2014-10-07 11:41:44 -0700

    Show and Hide jQuery

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
-//Hide warning
-//Show warning Slowly
+// //Hide warning
+// jQuery('.warning').hide();
+
+// //Show warning Slowly
+// // instead of writing jQuery, we can use $ for shortcut
+// $('.warning').show('slow');
+
+// Method chaining: can add one method after another:
+$('.warning').hide().show('show');
+
